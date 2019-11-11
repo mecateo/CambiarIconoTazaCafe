d411cb5c71b01c011166f6b77786da1bdadde4ca
Miguel Angel Meca Pacherres <miguel_meca01@hotmail.com>
2019-11-11 17:27:02 -0500

    Cambiar el icono de la taza de cafe en java

---
+++ src/cambiariconotazacafe/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cambiariconotazacafe;
+
+import com.miguelmeca.formularios.JFrame_Principal;
+
+/**
+ *
+ * @author Miguel Angel Meca Pacherres <miguelmeca01@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame_Principal principal = new JFrame_Principal();
+        principal.setVisible(true);
+    }
+
+    
+
+}
